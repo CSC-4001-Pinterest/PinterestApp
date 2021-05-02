d1be5c9f9c3e6a9c3e3f8b5aca2e73c5c458eda9
krystal602 <kdaye117@rams.wssu.edu>
2021-05-02 18:36:03 -0400

    trying to get posts fragment to work

--- app/src/main/java/com/example/pinterestapp/MainActivity.java
+++ app/src/main/java/com/example/pinterestapp/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.pinterestapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+import android.view.MenuItem;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
+    // Using ArrayList to store images data
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Getting reference of recyclerView
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        // Setting the layout as Staggered Grid for vertical orientation
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+
+
+        // Sending reference and data to Adapter
+       Adapter adapter = new Adapter(MainActivity.this);
+
+        // Setting Adapter to RecyclerView
+      recyclerView.setAdapter(adapter);
     }
 }
